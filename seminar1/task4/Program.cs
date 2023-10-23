ea4b70ba79bb7fa277b0489f89ab61484d319571
Ivan Lintsov <ivan.linzov@gmail.com>
2023-10-23 22:24:10 +0300

    добавлена 4 дз семинара 1

---
+++ seminar1/task4/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 4: Напишите программу, которая 
+//на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 => 2, 4
+//8 => 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 2)
+{
+    Console.WriteLine("Нет четных чисел в указанном промежутке");
+}
+else
+{
+    int current = 2;
+    int step = 2;
+
+    while(current < N - step)
+    {
+        Console.Write($"{current}, ");
+        current += step;
+    }
+
+    Console.WriteLine(current);
+}
