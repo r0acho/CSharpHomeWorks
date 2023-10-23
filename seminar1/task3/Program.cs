6c0a84716591bf60c8a0202ef121056e9694227d
Ivan Lintsov <ivan.linzov@gmail.com>
2023-10-23 22:17:55 +0300

    добавлена 3 дз семинара 1

---
+++ seminar1/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 3: Напишите программу, которая 
+//на вход принимает число и выдаёт,
+//является ли число чётным.
+
+//4  => да
+//-3 => нет
+//7  => нет
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}    
