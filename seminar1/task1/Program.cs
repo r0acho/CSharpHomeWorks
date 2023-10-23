2a0649a0a302778850b02cf7b43bbedbf782fa2c
Ivan Lintsov <ivan.linzov@gmail.com>
2023-10-23 22:12:13 +0300

    добавлена 1 задача 1 семинара

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите программу, которая 
+// на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a=5; b=7   => max=7,  min=5
+// a=2; b=10  => max=10, min=2
+// a=-9; b=-3 => max=-3, min=-9
+
+Console.WriteLine("Введите числа a и b через Enter");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max={a},\tmin={b}");
+}
+else if (b > a)
+{
+    Console.WriteLine($"max={b},\tmin={a}");
+}
+else
+{
+    Console.WriteLine("a = b");
+}
